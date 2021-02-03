ecdfa7f0725148c5cbd0af800e32db0bf7ec826f
JoannaCoPo <jococopo@gmail.com>
2021-02-03 14:54:09 -0700

    Incomplete code to my test

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,16 +1,20 @@
 class Restaurant
-  attr_reader :opening_time, :name, :dishes, :add_dish
+  attr_reader :opening_time, :name, :dishes, :add_dish, :open_for_lunch
   #my questions for later :)
   def initialize (opening_time, name, add_dish = "Burrata")
     @opening_time = opening_time #where is 11:30 comoing from in prompt?
     @name = name
-    @dishes = [add_dish] #as close as I'm going to get
+    @dishes = [] #as close as I'm going to get
     @closing_time = 8
     @add_dish = add_dish
+    @open_for_lunch = true
   end
 
   def closing_time(time)
     p  "#{opening_time.to_i + time}:00"
+  end
+
+  def open_for_lunch
   end
 
 end
